0989622861246a4917a9b24b589499a28397646f
Shannon McDonald <shannon.mcdonald46@gmail.com>
2014-10-17 10:32:46 -0700

    added name and password for database, wrote some unit tests

--- Parent-Teacher/test/models/parent_test.rb
+++ Parent-Teacher/test/models/parent_test.rb
@@ -4,4 +4,32 @@ class ParentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @parent = Parent.create
+  end
+
+  test "creates new parent" do
+    assert @parent, "Parent was not created."
+  end
+
+  test "can delete parent" do
+    assert @parent.destroy, "Parent was not destroyed."
+  end
+
+  test "can get a student" do
+    assert @parent.students, "Parent.students is not a truthy value"
+  end
+
+  test "gets multiple students" do
+    student1 = Student.create
+    student2 = Student.create
+    @parent.students.push(student1, student2)
+    assert @parent.students.length == 2, "Number of students attached to the parent does not equal 2." 
+  end
+
+  test "can set a student" do
+    assert_instance_of(Student, @parent.students.new, "An instance of a student could not be created through the parent.")
+  end
+
 end
