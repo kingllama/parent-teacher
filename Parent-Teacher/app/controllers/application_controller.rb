4c9dd7cbeb216646d93798f977eaed42793a9d65
Kieran Cormack <kcormack91@gmail.com>
2014-10-20 21:21:25 +0000

    railssetup

--- Parent-Teacher/app/controllers/application_controller.rb
+++ Parent-Teacher/app/controllers/application_controller.rb
@@ -1,20 +1,6 @@
 class ApplicationController < ActionController::Base
-  respond_to :json
+
   protect_from_forgery with: :exception
-
-def index
-    render layout: layout_name
-end
-
-private
-
-def layout_name
-    if params[:layout] == 0
-        false
-    else
-        'application'
-    end
-end
 
 
 end
