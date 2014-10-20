1347328f87a37ffa44f51eab2eb042771ac69acf
Shannon McDonald <shannon.mcdonald46@gmail.com>
2014-10-20 16:12:51 -0700

    teacher and parent can log in and out and go to respective pages (passwords pending)

--- Parent-Teacher/app/controllers/sessions_controller.rb
+++ Parent-Teacher/app/controllers/sessions_controller.rb
@@ -7,18 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     school = School.find_by(email: params[:email])
 
-    if user && user.authenticate(params[:password])
+    if user #&& user.authenticate(params[:password])
       session[:user_id] = user.id
       case user.type
         when "Parent"
-          redirect_to parent_path
+          redirect_to parent_path(user)
         when "Teacher"
-          redirect_to teacher_path
+          redirect_to teacher_path(user)
         else
-          redirect_to application_path, notice: "Unknown user type."
+          redirect_to root_path, notice: "Unknown user type."
       end
 
-    elsif school && school.authenticate(params[:password])
+    elsif school #&& school.authenticate(params[:password])
       session[:user_id] = school.id
 
     else
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to application_path, notice: "Adios!"
+    redirect_to root_path, notice: "Adios!"
   end
 
 end
