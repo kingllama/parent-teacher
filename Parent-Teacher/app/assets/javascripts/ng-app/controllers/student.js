79663e70b0f77214b4ca994fc38c84276b592a07
Daniel Harris <dh.harris183@gmail.com>
2014-10-20 15:14:36 -0700

    Commiting some changes

--- Parent-Teacher/app/assets/javascripts/ng-app/controllers/student.js
+++ Parent-Teacher/app/assets/javascripts/ng-app/controllers/student.js
@@ -1,3 +1,4 @@
 parentTeacherApp.controller('Student', function ($scope, $location, StudentFactory){
-  $scope.student = StudentFactory.query({}, function(){}, function(){})
+  $scope.student = StudentFactory.query({}, function(){}, function(){});
+  $scope.students = StudentFactory.show({id: /*??*/}, function(){}, function(){});
 })
