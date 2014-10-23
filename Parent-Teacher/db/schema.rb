747f8dc14468c08460ebae1f1292fc09d6f105f4
Kieran Cormack <kcormack91@gmail.com>
2014-10-23 00:09:06 +0000

    littlefix

--- Parent-Teacher/db/schema.rb
+++ Parent-Teacher/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141022184834) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +22,12 @@ ActiveRecord::Schema.define(version: 20141022184834) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "teacher_id"
     t.integer  "school_id"
   end
 
   add_index "classrooms", ["school_id"], name: "index_classrooms_on_school_id", using: :btree
+  add_index "classrooms", ["teacher_id"], name: "index_classrooms_on_teacher_id", using: :btree
 
   create_table "events", force: true do |t|
     t.string   "title"
