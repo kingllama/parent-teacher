314ea233510c9688e8318c8f1d535882cc1e6551
Kieran Cormack <kcormack91@gmail.com>
2014-10-26 01:11:37 +0000

    cleaning up

--- Parent-Teacher/public/chat.js
+++ Parent-Teacher/public/chat.js
@@ -25,13 +25,13 @@ chat.channel.bind('message_event', function(incomingData){
   var parent_p = $("<p>");
   var teacher_p = $("<p>");
   if (incomingData.userType === "Teacher"){
-    teacher_p.addClass('teacher-msg-name').text(incomingData.userName);
-    teacher_p.addClass('teacher-msg').text(incomingData.message);
-    div.addClass('teacher-bubble').append(teacher_p);
+    teacher_p.addClass('teacher-msg-name').text(incomingData.userName)
+    teacher_p.addClass('teacher-msg').text(incomingData.message)
+    div.append(teacher_p).appendTo('#chat-box');
   } else {
-    parent_p.addClass('parent-msg-name').text(incomingData.userName);
-    parent_p.addClass('parent-msg').text(incomingData.message);
-    div.addClass('parent-bubble').append(parent_p);
+    parent_p.addClass('parent-msg-name').text(incomingData.userName)
+    parent_p.addClass('parent-msg').text(incomingData.message)
+    div.append(parent_p).appendTo('#chat-box');
   };
 });
 
@@ -44,4 +44,6 @@ $('button').on('click', function(){
   chat.sendMessage(messageData);
   $('#input').val("");
 });
+
+
 
