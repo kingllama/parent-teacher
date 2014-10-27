7fadd6650c3251c1332fff0a91fd616fd92af397
Daniel Harris <dh.harris183@gmail.com>
2014-10-27 14:44:22 -0700

    Chat bug fix

--- Parent-Teacher/public/chat.js
+++ Parent-Teacher/public/chat.js
@@ -4,6 +4,7 @@ var messageData = {
   message: ''
 };
 
+
 var chat = {
   dispatcher: null,
   channel: null,
@@ -18,22 +19,8 @@ var chat = {
   }
 };
 
-chat.connect();
 
-chat.channel.bind('message_event', function(incomingData){
-  var div = $("<div>").addClass("speech-bubble");
-  var parent_p = $("<p>");
-  var teacher_p = $("<p>");
-  if (incomingData.userType === "Teacher"){
-    teacher_p.addClass('teacher-msg-name').text(incomingData.userName)
-    teacher_p.addClass('teacher-msg').text(incomingData.message)
-    div.append(teacher_p).appendTo('#chat-box');
-  } else {
-    parent_p.addClass('parent-msg-name').text(incomingData.userName)
-    parent_p.addClass('parent-msg').text(incomingData.message)
-    div.append(parent_p).appendTo('#chat-box');
-  };
-});
+
 
 var getInput = function(){
   return $('#input').val();
@@ -43,6 +30,24 @@ $('button').on('click', function(){
   messageData.message = getInput();
   chat.sendMessage(messageData);
   $('#input').val("");
+});
+
+chat.connect();
+
+chat.channel.bind('message_event', function(incomingData){
+  var div = $("<div>")
+  if (incomingData.userType === "Teacher"){
+    div.addClass("teacher-bubble");
+    $("<p>").addClass('teacher-msg-name').text(incomingData.userName).appendTo(div)
+    $("<p>").addClass('teacher-msg').text(incomingData.message).appendTo(div)
+    div.appendTo('#chat-box');
+  } else {
+    div.addClass("parent-bubble");
+    $("<p>").addClass('parent-msg-name').text(incomingData.userName).appendTo(div)
+    $("<p>").addClass('parent-msg').text(incomingData.message).appendTo(div)
+    div.appendTo('#chat-box');
+  };
+  $("#chat-box").animate({ scrollTop: 999999999 }, "slow");
 });
 
 
